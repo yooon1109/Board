4ac98d6f472ba824043bee44ca48525a92aa577a
yooon <jys_1109@naver.com>
2024-03-20 11:12:45 +0900

    로컬 > aws 서버로 변경

--- src/main/frontend/src/setProxy.js
+++ src/main/frontend/src/setProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:8080',
+            target: 'http://ec2-3-229-105-151.compute-1.amazonaws.com:8080/',
     changeOrigin: true,
 })
 );
